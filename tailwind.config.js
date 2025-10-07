46cd9b734ab4e5cd3e8b4eab7ee10995725eb5f1
Bahran Solomon tadesse <bahran.solomon@astu.edu.et>
2025-10-07 15:24:20 +0300

    add portfolio section to main page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
-// tailwind.config.js
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
